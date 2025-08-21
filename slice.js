9b83e771a45544964b3a227f6b5f210566689292
MD NAZMUL HASAN <80097373+Shondarzer-Taroka@users.noreply.github.com>
2025-08-21 16:34:52 +0600

    update slice.js

--- slice.js
+++ slice.js
@@ -3,3 +3,5 @@ const arr=[1,2,3,4,5,6,7,8]
 
 const newArr=arr.slice(0,3)
 console.log(newArr)
+
+// slice method is one of the methods, it returns the slected elements as new array, it selects element from starting elements and untill ends. if we probide starting and ending pont. it slice according to the points.
