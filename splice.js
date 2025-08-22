bad76a38755352239570c8b72c4d2880c2d79b2a
MD NAZMUL HASAN <80097373+Shondarzer-Taroka@users.noreply.github.com>
2025-08-22 14:05:22 +0000

    splice adding items

--- splice.js
+++ splice.js
@@ -1,7 +1,10 @@
 
 const arr=[1,2,3,45,6,7,47]
 
+const addingItems= arr.splice(3, 1, "a", "b", "c");
 const newArr=arr.splice(0,3)
 console.log(newArr);
 
 
+console.log(addingItems);
+console.log(arr);
